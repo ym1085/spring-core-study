5749e40b78812a82846eb0bc36773a5033350abb
ymkim <ymkim1085@funin.camp>
2023-02-19 16:04:12 +0900

    feat: 싱글톤 컨테이너 패턴 사용
    * 기존 pureSingleton pattern을 spring과 엮어서 테스트

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,7 +12,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void test() throws Exception {
+    void pureContainer() throws Exception {
         //given
         AppConfig appConfig = new AppConfig();
 
@@ -57,4 +58,24 @@ public class SingletonTest {
         // equal : java equals method
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤 테스트")
+    void springContainer() throws Exception {
+        //given
+        //AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //when
+        //then
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        assertThat(memberService1).isSameAs(memberService2);
+        //assertThat(memberService1).isNotSameAs(memberService2);
+    }
 }
