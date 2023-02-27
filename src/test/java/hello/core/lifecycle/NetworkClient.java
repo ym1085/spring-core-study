879fd695c85374d1a6c42e0f30996d62af475ac6
ymkim <ymkim1085@funin.camp>
2023-02-27 23:26:56 +0900

    feat: Bean lifecycle 관련 테스트 코드 및 예제 작성

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,33 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    //Network connect URL
+    private String url;
+
+    //기본 생성자
+    public NetworkClient() {
+        System.out.println("생서자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //외부에서 셋팅이 가능하도록 setter 생성
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call = " + url + ", message = " + message);
+    }
+
+    //서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close = " + url);
+    }
+}
