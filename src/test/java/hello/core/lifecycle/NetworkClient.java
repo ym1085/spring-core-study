704b4ff240663dfc149a6c745226b21ee4bdf8ce
ymkim <ymkim1085@funin.camp>
2023-02-28 22:27:47 +0900

    feat: Bean lifecycle 관련 초기화 소멸 메서드 호출
    * @Bean(initMethod = "init", destroyMethod = "close") 사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     //Network connect URL
     private String url;
@@ -35,17 +32,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입이 끝나면 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    //
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.disconnect");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
